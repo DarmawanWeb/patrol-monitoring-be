48e616357be49bdbd1b015a4d01d53bc71da31ad
agus-darmawan <wayanagus.dr@gmail.com>
2025-06-14 04:29:48 +0700

    feat: add get image from uploads folder

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import { setupMiddleware, setupErrorHandlers } from '@middleware/index.js';
 import { Request, Response } from 'express';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import v1Routes from '@/routes/index.js';
 
 const createApp = (): express.Application => {
   const app = express();
+
+  // Mendapatkan __dirname dengan ES module
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
 
   // Setup middleware
   setupMiddleware(app);
@@ -13,12 +18,17 @@ const createApp = (): express.Application => {
   // Routes
   app.use('/api', v1Routes);
 
+  // Route utama
   app.get('/', (req: Request, res: Response) => {
     res.status(200).json({
       message: 'Welcome to the API',
       timestamp: new Date().toISOString(),
     });
   });
+
+  // Menyajikan file statis dari folder uploads
+  const uploadPath = path.join(__dirname, '../uploads');
+  app.use('/uploads', express.static(uploadPath));
 
   // Error handlers
   setupErrorHandlers(app);
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+const router: Router = Router();
+
 import healthRoutes from './health.routes.js';
 import authRoutes from './auth.routes.js';
 import patrolArea from './patrol-area.routes.js';
-const router: Router = Router();
 
 router.use('/health', healthRoutes);
 router.use('/auth', authRoutes);
