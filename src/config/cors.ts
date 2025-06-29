a2648e07f0280d4296e67b859263bb5a7963ac67
agus-darmawan <wayanagus.dr@gmail.com>
2025-06-29 22:35:24 +0700

    fix: cors error by disable https

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,12 +3,8 @@ import { env } from '@config/env.js';
 
 export const corsConfig = cors({
   origin: env.cors.origin,
-  credentials: true,
+  credentials: false,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 
-  allowedHeaders: [
-    'Content-Type',
-    'Authorization',
-    'Cross-Origin-Resource-Policy',
-  ],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 });
